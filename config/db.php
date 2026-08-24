8fad5992728761f12705f7f896f5c2c1c59a3511
Roman <roman8610@gmail.com>
2026-08-24 14:52:55 +0300

    Исправлено подключение к БД

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=test',
-    'username' => 'root1',
+    'dsn' => 'mysql:host=127.0.1.12;dbname=test',
+    'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
 
